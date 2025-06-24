826e4c84faac32d6b4fa1cb4073795227a24b61b
Boyd Bloemsma <boyd.bloemsma@eqom.com>
2025-06-24 17:57:07 +0200

    feat: using new image optimizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  site: 'https://boydbloemsma.com',
-  trailingSlash: 'never',
+  site: "https://boydbloemsma.com",
+  trailingSlash: "never",
   integrations: [react(), sitemap()],
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
   markdown: {
     shikiConfig: {
       theme: "gruvbox-light-hard",
-    }
-  }
+    },
+  },
 });
